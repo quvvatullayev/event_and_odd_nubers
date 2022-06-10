e792c597a8377908952389815a419e4445dfa324
tajriba <quvvatullayev@gmail.com>
2022-06-10 21:21:26 +0300

    number_even javobi

--- number_even.py
+++ number_even.py
@@ -2,4 +2,15 @@
 
 #Create a variable "var_int" and assign it a four-digit integer value.
 
+
+var_int = 7896
+
+
 #Print the number of even digits in the variable "var_int".
+
+x1 = var_int%10
+x2 = var_int%100//10
+x3 = var_int//100%10
+x4 = var_int//1000
+
+print(x1%2, x2%2, x3%2, x4%2, sep="\n")
