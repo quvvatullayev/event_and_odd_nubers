f76f39fdb1da2b5672916a43cf794e80c3d6cd92
tajriba <quvvatullayev@gmail.com>
2022-06-13 16:19:48 +0300

    sum_even.py ni javoblari

--- sum_even.py
+++ sum_even.py
@@ -1,36 +1,16 @@
 #A four-digit integer is given. Find the sum of even digits in it.
-
-
-
-
-
 #Create a variable "var_int" and assign it a four-digit integer value.
-
-
-var_int = 8619
-
-
-
 #Create a variable "sum_even" and assign it 0.
-
-
-
-sum_even = 0
-
-
-
 #Find the sum of the even digits in the variable "var_int".
 
+var_int = 8622
 
+sum_even = 0
 
 x1 = var_int%10
 x2 = var_int%100//10
 x3 = var_int//100%10
 x4 = var_int//1000
-
-
-
-
 
 sum_even = (x1+1)%2*x1 + (x2+1)%2*x2 + (x3+1)%2*x3 + (x4+1)%2*x4
 
