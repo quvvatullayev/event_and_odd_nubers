1c4c0267fe87b3f8c1d750e46dd8a417dc060d34
tajriba <quvvatullayev@gmail.com>
2022-06-13 08:38:53 +0300

    is_odd ni javob

--- is_odd.py
+++ is_odd.py
@@ -1,6 +1,12 @@
 #Create a variable "number" and assign it a integer value.
 
-#Print 1 if the number is odd, otherwise print 0.
+
 number = 41
+
+
+#Print 1 if the number is odd, otherwise print 0.
+
+
+
 
 print(number%2)
